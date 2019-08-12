d8a4cd8f4f96548cdce725ba4d65981f1ee03e8f
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-12 20:53:02 +0000

    coding exercises

---
+++ javascriptExercise.js
@@ -0,0 +1,45 @@
+//Basic JavaScript Cards
+
+// var count = 0;
+
+// function cc(card) {
+//   // Only change code below this line
+//   if(card >= 2 && card <= 6) {
+//     count++
+    
+//   } else if(card >= 7 && card <= 9) {
+    
+//   } else {
+//     count--
+//   }
+  
+//   if (count > 0) {
+//     return count + " Bet" 
+//   } else { 
+//     return count + " Hold"
+//   }
+//   // Only change code above this line
+// }
+
+// // Add/remove calls to test your function.
+// // Note: Only the last will display
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Basic JavaScript Objects
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Cat",
+  "legs": 10,
+  "tails": 20,
+  "friends": ["Doggomon", "Cow"],
+};
