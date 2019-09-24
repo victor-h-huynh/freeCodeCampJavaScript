74e0ba5dd60f2595abef8df188fb18e358fdf671
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 19:44:01 -0400

    Basic JavaScript: Nesting For Loops

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -541,3 +541,24 @@
 // for (let i = 0; i < myArr.length; i++) {
 //   total += myArr[i]
 // }
+
+//////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 24TH 2019 Basic JavaScript: Nesting For Loops //
+/////////////////////////////////////////////////////////////////////////////////////
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
