fc3970d987b0e31cd53d671a05cee13a2f4dff65
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 18:30:29 -0400

    Basic JavaScript: Iterate with JavaScript While Loops

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -401,45 +401,60 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 // Setup
-var collection = {
-  "2548": {
-    album: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: ["Let It Rock", "You Give Love a Bad Name"]
-  },
-  "2468": {
-    album: "1999",
-    artist: "Prince",
-    tracks: ["1999", "Little Red Corvette"]
-  },
-  "1245": {
-    artist: "Robert Palmer",
-    tracks: []
-  },
-  "5439": {
-    album: "ABBA Gold"
-  }
-};
-// Keep a copy of the collection for tests
-var collectionCopy = JSON.parse(JSON.stringify(collection));
+// var collection = {
+//   "2548": {
+//     album: "Slippery When Wet",
+//     artist: "Bon Jovi",
+//     tracks: ["Let It Rock", "You Give Love a Bad Name"]
+//   },
+//   "2468": {
+//     album: "1999",
+//     artist: "Prince",
+//     tracks: ["1999", "Little Red Corvette"]
+//   },
+//   "1245": {
+//     artist: "Robert Palmer",
+//     tracks: []
+//   },
+//   "5439": {
+//     album: "ABBA Gold"
+//   }
+// };
+// // Keep a copy of the collection for tests
+// var collectionCopy = JSON.parse(JSON.stringify(collection));
 
-// Only change code below this line
+// // Only change code below this line
 
-function updateRecords(id, prop, value) {
-  if (prop === "tracks" && value !== "") {
-    if (collection[id][prop]) {
-      collection[id][prop].push(value);
-    } else {
-      collection[id][prop] = [value];
-    }
-  } else if (value !== "") {
-    collection[id][prop] = value;
-  } else {
-    delete collection[id][prop];
-  }
+// function updateRecords(id, prop, value) {
+//   if (prop === "tracks" && value !== "") {
+//     if (collection[id][prop]) {
+//       collection[id][prop].push(value);
+//     } else {
+//       collection[id][prop] = [value];
+//     }
+//   } else if (value !== "") {
+//     collection[id][prop] = value;
+//   } else {
+//     delete collection[id][prop];
+//   }
 
-  return collection;
+//   return collection;
+// }
+
+// // Alter values below to test your code
+// updateRecords(5439, "artist", "ABBA");
+
+//////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 24TH 2019 Basic JavaScript: Iterate with JavaScript While Loops //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i = 0;
+while (i < 5) {
+  myArray.push(i);
+  i++;
 }
-
-// Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
