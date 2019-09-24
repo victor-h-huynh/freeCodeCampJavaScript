2ae99e10eda56b241bc5aa8bc84f8775cd9c3136
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 16:11:33 -0400

    Basic JavaScript: Using Objects for Lookups

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -249,3 +249,54 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // SEPTEMBER 24TH 2019 Basic JavaScript: Delete Properties from a JavaScript Object //
 /////////////////////////////////////////////////////////////////////////////////////
+
+// Example
+// var ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"],
+//   "bark": "bow-wow"
+// };
+
+// delete ourDog.bark;
+
+// // Setup
+// var myDog = {
+//   "name": "Happy Coder",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["freeCodeCamp Campers"],
+//   "bark": "woof"
+// };
+
+// // Only change code below this line.
+
+// delete myDog.tails
+
+//////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 24TH 2019 Basic JavaScript: Using Objects for Lookups                 //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank"
+  };
+
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
