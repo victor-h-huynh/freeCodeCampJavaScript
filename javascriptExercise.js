6da8f860880fdaef6bd3e45e3a7afade23389c0b
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 17:02:07 -0400

    Basic JavaScript: Testing Objects for Properties

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -279,24 +279,28 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 // Setup
-function phoneticLookup(val) {
-  var result = "";
+// function phoneticLookup(val) {
+//   var result = "";
 
-  // Only change code below this line
-  var lookup = {
-    alpha: "Adams",
-    bravo: "Boston",
-    charlie: "Chicago",
-    delta: "Denver",
-    echo: "Easy",
-    foxtrot: "Frank"
-  };
+//   // Only change code below this line
+//   var lookup = {
+//     alpha: "Adams",
+//     bravo: "Boston",
+//     charlie: "Chicago",
+//     delta: "Denver",
+//     echo: "Easy",
+//     foxtrot: "Frank"
+//   };
 
-  result = lookup[val];
+//   result = lookup[val];
 
-  // Only change code above this line
-  return result;
-}
+//   // Only change code above this line
+//   return result;
+// }
 
-// Change this value to test
-phoneticLookup("charlie");
+// // Change this value to test
+// phoneticLookup("charlie");
+
+//////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 24TH 2019 Testing Objects for Properties //
+/////////////////////////////////////////////////////////////////////////////////////
