9ce74cef19d44f2670c46c4569b25a36441f9e13
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 15:31:16 -0400

    access object property

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -97,3 +97,15 @@
 // }
 
 // diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// Setup
+// var testObj = {
+//   "hat": "ballcap",
+//   "shirt": "jersey",
+//   "shoes": "cleats"
+// };
+
+// // Only change code below this line
+
+// var hatValue = testObj.hat;      // Change this line
+// var shirtValue = testObj.shirt;    // Change this line
