f812b3ab2fce7a9874afc63b611d220022b079e9
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-24 20:51:03 -0400

    Basic JavaScript: Iterate with JavaScript Do...While Loops

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -546,19 +546,34 @@
 // SEPTEMBER 24TH 2019 Basic JavaScript: Nesting For Loops //
 /////////////////////////////////////////////////////////////////////////////////////
 
-function multiplyAll(arr) {
-  var product = 1;
-  // Only change code below this line
+// function multiplyAll(arr) {
+//   var product = 1;
+//   // Only change code below this line
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      product = product * arr[i][j];
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       product = product * arr[i][j];
+//     }
+//   }
 
-  // Only change code above this line
-  return product;
-}
+//   // Only change code above this line
+//   return product;
+// }
 
-// Modify values below to test your code
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+// // Modify values below to test your code
+// multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 24TH 2019 Basic JavaScript: Iterate with JavaScript Do...While Loops //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
