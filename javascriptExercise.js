4d5bb97cea78cf9af21d607b5bb9c18ecfd6e37c
Victor Huynh <victor.h.huynh@gmail.com>
2019-09-30 18:36:30 -0400

    function randomFraction() {
      // Only change code below this line.
    
      return Math.random();
    
      // Only change code above this line.
    
    ]

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -579,5 +579,61 @@
 // } while (i < 5);
 
 //////////////////////////////////////////////////////////////////////////////////////
+// SEPTEMBER 30TH 2019 Basic JavaScript: Generate Random Fractions with JavaScript //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// function randomFraction() {
+//   // Only change code below this line.
+
+//   return Math.random();
+
+//   // Only change code above this line.
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////
 // SEPTEMBER 30TH 2019 Basic JavaScript: Profile Lookup //
 /////////////////////////////////////////////////////////////////////////////////////
+
+//Setup
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"]
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name && contacts[i].hasOwnProperty(prop)) {
+      return contacts[i][prop];
+    } else {
+      return "No such property";
+    }
+  }
+  return "No such contact";
+  // Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Kristian", "lastName");
