144df18d54f11f32d4e67162433bfcdcf8318572
Victor Huynh <victor.h.huynh@gmail.com>
2019-10-01 16:09:20 -0400

    ES6: Write Higher Order Arrow Functions

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -810,9 +810,27 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // OCTOBER 1ST 2019 ES6: Write Arrow Functions with Parameters //
 /////////////////////////////////////////////////////////////////////////////////////
-const myConcat = (arr1, arr2) => {
-  arr1.concat(arr2);
-};
 
+// const myConcat = (arr1, arr2) => {
+//   arr1.concat(arr2);
+// };
+
+// // test your code
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+//////////////////////////////////////////////////////////////////////////////////////
+// OCTOBER 1ST 2019 ES6: ES6: Write Higher Order Arrow Functions //
+/////////////////////////////////////////////////////////////////////////////////////
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = arr => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr
+    .filter(num => num > 0 && num % parseInt(num) === 0)
+    .map(num => Math.pow(num, 2));
+  // change code above this line
+  return squaredIntegers;
+};
 // test your code
-console.log(myConcat([1, 2], [3, 4, 5]));
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
