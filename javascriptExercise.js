40dcce4b6778b2736684b9aa8bf05e4b8024dcba
Victor Huynh <victor.h.huynh@gmail.com>
2019-10-01 15:19:59 -0400

    ES6: Mutate an Array Declared with const

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -743,3 +743,35 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // OCTOBER 1ST 2019 ES6: Declare a Read-Only Variable with the const Keyword //
 /////////////////////////////////////////////////////////////////////////////////////
+
+// function printManyTimes(str) {
+//   "use strict";
+
+//   // change code below this line
+
+//   const SENTENCE = str + " is cool!";
+//   for(let i = 0; i < str.length; i+=2) {
+//     console.log(SENTENCE);
+//   }
+
+//   // change code above this line
+
+// }
+// printManyTimes("freeCodeCamp");
+
+//////////////////////////////////////////////////////////////////////////////////////
+// OCTOBER 1ST 2019 ES6: Mutate an Array Declared with const //
+/////////////////////////////////////////////////////////////////////////////////////
+
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // change code above this line
+}
+editInPlace();
