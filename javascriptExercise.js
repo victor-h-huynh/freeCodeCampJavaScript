0f5150edb5223a0875c6a8e0e76d251a0cd37b74
Victor Huynh <victor.h.huynh@gmail.com>
2019-10-01 15:26:51 -0400

    ES6: Prevent Object Mutation

--- javascriptExercise.js
+++ javascriptExercise.js
@@ -763,15 +763,38 @@
 // OCTOBER 1ST 2019 ES6: Mutate an Array Declared with const //
 /////////////////////////////////////////////////////////////////////////////////////
 
-const s = [5, 7, 2];
-function editInPlace() {
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   "use strict";
+//   // change code below this line
+
+//   s[0] = 2;
+//   s[1] = 5;
+//   s[2] = 7;
+
+//   // change code above this line
+// }
+// editInPlace();
+
+//////////////////////////////////////////////////////////////////////////////////////
+// OCTOBER 1ST 2019 ES6: Prevent Object Mutation //
+/////////////////////////////////////////////////////////////////////////////////////
+
+function freezeObj() {
   "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
   // change code below this line
 
-  s[0] = 2;
-  s[1] = 5;
-  s[2] = 7;
+  Object.freeze(MATH_CONSTANTS);
 
   // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
 }
-editInPlace();
+const PI = freezeObj();
